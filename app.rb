3dacddf677f19b007588dbd83891bed16c4d2e0d
Dennis Rodriguez <dennis@ufn.com>
2017-08-09 13:15:35 -0400

    Addition of post endpoint and form in view

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'sinatra'
 require 'diceware'
 
 get '/' do
-    gen = Diceware::GeneratePassphrase.new
-    genRequest = Diceware::GeneratePassphraseRequest.new(6)
-    passphrase = gen.generate(genRequest)
-    erb :index, :locals => {:passphrase => passphrase}
+  gen = Diceware::GeneratePassphrase.new
+  genRequest = Diceware::GeneratePassphraseRequest.new(6)
+  passphrase = gen.generate(genRequest)
+  erb :index, :locals => {:passphrase => passphrase, :num_words => '6'}
+end
+
+post '/' do
+  gen = Diceware::GeneratePassphrase.new
+  genRequest = Diceware::GeneratePassphraseRequest.new(params[:num_words].to_i)
+  passphrase = gen.generate(genRequest)
+  erb :index, :locals => {:passphrase => passphrase, :num_words => params[:num_words]}
 end
