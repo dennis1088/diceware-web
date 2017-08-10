c222e79ed3ff929177ff473d98d7ffba88b8adda
Dennis Rodriguez <dennis@ufn.com>
2017-08-10 08:40:17 -0400

    Fixing rubocop offenses

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require 'diceware'
 
 get '/' do
   gen = Diceware::GeneratePassphrase.new
-  genRequest = Diceware::GeneratePassphraseRequest.new(6)
-  passphrase = gen.generate(genRequest)
-  erb :index, :locals => {:passphrase => passphrase, :num_words => '6'}
+  gen_request = Diceware::GeneratePassphraseRequest.new(6)
+  passphrase = gen.generate(gen_request)
+  erb :index, locals: { passphrase: passphrase, num_words: '6' }
 end
 
 post '/' do
   gen = Diceware::GeneratePassphrase.new
-  genRequest = Diceware::GeneratePassphraseRequest.new(params[:num_words].to_i)
-  passphrase = gen.generate(genRequest)
-  erb :index, :locals => {:passphrase => passphrase, :num_words => params[:num_words]}
+  gen_request = Diceware::GeneratePassphraseRequest.new(params[:num_words].to_i)
+  passphrase = gen.generate(gen_request)
+  erb :index, locals: { passphrase: passphrase, num_words: params[:num_words] }
 end
