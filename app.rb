8b7674f9b72a810417f433b1b0323c191b15bdc6
Dennis Rodriguez <dennis@ufn.com>
2017-08-10 13:51:45 -0400

    Addition of other settings

--- app.rb
+++ app.rb
@@ -9,8 +9,11 @@ get '/' do
 end
 
 post '/' do
+  num_words = params[:num_words].to_i
+  capitalize = params[:capitalize] == 'on' ? true : false
+  add_digit = params[:add_digit] == 'on' ? true : false
   gen = Diceware::GeneratePassphrase.new
-  gen_request = Diceware::GeneratePassphraseRequest.new(params[:num_words].to_i)
+  gen_request = Diceware::GeneratePassphraseRequest.new(num_words, capitalize, add_digit)
   passphrase = gen.generate(gen_request)
-  erb :index, locals: { passphrase: passphrase, num_words: params[:num_words] }
+  erb :index, locals: { passphrase: passphrase, num_words: params[:num_words], capitalize: params[:capitalize], num_words: params[:add_digit] }
 end
