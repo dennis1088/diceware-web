03795a32bd6a6eb7b9d8fe5ecb574fd3a9b988f8
Dennis Rodriguez <dennis1088@gmail.com>
2018-02-04 12:59:12 -0500

    Finishing settings and added some styling

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'diceware'
 
 get '/' do
   gen = Diceware::GeneratePassphrase.new
   gen_request = Diceware::GeneratePassphraseRequest.new(6)
   passphrase = gen.generate(gen_request)
-  erb :index, locals: { passphrase: passphrase, num_words: '6' }
+  erb :index, locals: { passphrase: passphrase, num_words: '6', capitalize: 'off', add_digit: 'off' }
 end
 
 post '/' do
@@ -15,5 +16,5 @@ post '/' do
   gen = Diceware::GeneratePassphrase.new
   gen_request = Diceware::GeneratePassphraseRequest.new(num_words, capitalize, add_digit)
   passphrase = gen.generate(gen_request)
-  erb :index, locals: { passphrase: passphrase, num_words: params[:num_words], capitalize: params[:capitalize], num_words: params[:add_digit] }
+  erb :index, locals: { passphrase: passphrase, num_words: params[:num_words], capitalize: params[:capitalize], add_digit: params[:add_digit] }
 end
