8cd3be4b03ecb08d346b46ffdc067f257aa31a5f
Dennis Rodriguez <dennis1088@gmail.com>
2018-02-13 21:18:16 -0500

    Addition of rake task and first test file

---
+++ test/diceware_test.rb
@@ -0,0 +1,15 @@
+require File.expand_path '../test_helper.rb', __FILE__
+
+class DicewareTest < MiniTest::Unit::TestCase
+
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_hello_world
+    get '/'
+    assert last_response.ok?
+  end
+end
